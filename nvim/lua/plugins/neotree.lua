b385efc2cc1053c25dfe25741ec741a66fa41675
fabiomnk <fabiomnc@gmail.com>
2024-05-21 14:19:35 +0200

    nvim neotree show hidden files

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,12 +1,22 @@
-return  {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ':Neotree filesystem reveal left<CR>', {})
-  end
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v2.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		local config = require("neo-tree")
+		config.setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+	end,
 }
