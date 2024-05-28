6c82d78a4ba00dcfd2fbb400f402d8f448350f94
fabiomnk <fabiomnc@gmail.com>
2024-05-29 00:31:02 +0200

    edit nvim config & add lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,19 +5,23 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "light"
 vim.cmd("set number")
+vim.cmd("set norelativenumber")
+
+--vim.opt.number = true
+--vim.opt.relativenumber = false
+--vim.opt.signcolumn = "number"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
 
 require("lazy").setup("plugins")
