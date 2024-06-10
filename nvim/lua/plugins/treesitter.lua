2b8ca62ffa0f6d181ab7b69cda3617a35a23b142
Fabio Mancini <fabiomnc@gmail.com>
2024-06-10 11:41:35 +0200

    fix: update configs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-          ensure_installed = { "terraform","lua","bash" },
+          ensure_installed = { "terraform","lua","bash","json","yaml","go","rego","robot" },
           auto_install = true,
           highlight = { enable = true },
           indent = { enable = true }
